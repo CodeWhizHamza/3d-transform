833c543b46065d4ea0a1f2e742ecc06b3da8a43e
Hamza Dev <muhammadhamzam1486@gmail.com>
2021-07-24 14:45:32 +0500

    Link GitHub

---
+++ components/Footer.jsx
@@ -0,0 +1,52 @@
+import { primaryColor, primaryColorLight } from '../config/config'
+
+export default function Footer() {
+  return (
+    <div className="container">
+      <footer className="footer">
+        <p className="footer__copyright">Copyright &copy; Hamza 2021</p>
+        <a
+          href="http://github.com/hamza-student"
+          className="footer__link"
+          target="_blank"
+        >
+          See my github
+        </a>
+      </footer>
+
+      <style jsx>
+        {`
+          .container {
+            background-color: ${primaryColor};
+            color: #fff;
+          }
+          .footer {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            max-width: 1366px;
+            padding: 0.5em 1em;
+            margin: auto;
+            margin-top: 1em;
+          }
+          .footer__link {
+            text-decoration: none;
+            color: #fff;
+            transition: transform 0.3s ease;
+            display: block;
+            padding: 0.5em 1.5em;
+          }
+          .footer__link:hover {
+            transform: rotate(-10deg) scale(1.15);
+            background-image: linear-gradient(
+              to right,
+              ${primaryColor},
+              ${primaryColorLight},
+              ${primaryColor}
+            );
+          }
+        `}
+      </style>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import MainHead from '../components/MainHead'
 import Nav from '../components/Nav'
 import Main from '../components/Main'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <MainHead />
       <Nav />
       <Main />
+      <Footer />
     </>
   )
 }
